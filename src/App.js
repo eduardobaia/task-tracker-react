3329b649cf04909693037e73b5e60165030ef3d3
Eduardo Baia <eduardobaia@Eduardos-Air.Home>
2022-03-25 15:41:20 +0000

    react task tracker with json server

--- src/App.js
+++ src/App.js
@@ -1,23 +1,75 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from "./components/Header";
+import Task from "./components/Tasks";
+import { useState, useEffect } from "react";
+import AddTask from "./components/AddTask";
 
 function App() {
+  const [showAddTask, setShowAddTask] = useState(false);
+
+  useEffect(() => {
+    const getTasks = async () => {
+      const tasksFromServer = await fetchTasks();
+      setTasks(tasksFromServer);
+    };
+    getTasks();
+  }, []);
+
+  //fetch dats from taskjs
+  const fetchTasks = async () => {
+    const res = await fetch("http://localhost:8080/tasks");
+    const data = await res.json();
+
+    return data;
+  };
+
+  const [tasks, setTasks] = useState([]);
+
+  //delete tasks
+  const deleteTaskOld = (id) => {
+    setTasks(tasks.filter((task) => task.id !== id));
+    console.log("porran ", id);
+  };
+
+  //toggle reminder
+  const toggleReminder = (id) => {
+    setTasks(
+      tasks.map((task) =>
+        task.id === id ? { ...task, reminder: !task.reminder } : task
+      )
+    );
+  };
+
+  //Add task
+  const addTask = (task) => {
+    console.log(task);
+    const id = Math.floor(Math.random() * 10000) + 1;
+
+    const newTask = { id, ...task };
+    setTasks([...tasks, newTask]);
+  };
+
+  //delete task
+  const deleteTask = async (id) => {
+    await fetch(`http://localhost:8080/tasks/${id}`, {
+      method: "DELETE",
+    });
+    setTasks(tasks.filter((task) => task.id !== id));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <Header
+        onAdd={() => setShowAddTask(!showAddTask)}
+        showAdd={showAddTask}
+      />
+
+      {showAddTask && <AddTask onAdd={addTask} />}
+
+      {tasks.length > 0 ? (
+        <Task tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />
+      ) : (
+        "No tasks "
+      )}
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,30 @@
+import PropTypes from "prop-types";
+import { Button } from "./Button";
+
+const Header = ({ title, onAdd, showAdd }) => {
+  const onClick = () =>{
+    console.log("CLICKEEE ");
+  }
+  return (
+    <header className="header">
+      <h1>{title}</h1>
+      {/* <h1 style={headingStyle}>{title}</h1> */}
+      <Button onClick={onAdd} color={!showAdd ? 'green': 'red'} text={!showAdd ? 'Add': 'Close'} />
+    
+    </header>
+  );
+};
+Header.defaultProps = {
+  title: "Task Tracker.",
+};
+
+Header.protoTypes = {
+  title: PropTypes.string,
+};
+
+// const headingStyle={
+//   color:'blue',
+//   backgroundColor:'black'
+// }
+
+export default Header;
